53f3ed1c0f698141a4a36e3c61b97efa17fb3fbb
Caroline Forslund <cf222ev@student.lnu.se>
2014-11-08 15:12:40 +0000

    vandalen - everything except errrorhandling

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,28 @@
 
 var makePerson = function(persArr){
 
+    var persons;
+    
+    persons = {
+        minAge: function (){
+            return Math.min.apply(Math,persArr.map(function(persArr){return persArr.age}));
+        },
+        maxAge: function (){
+            return Math.max.apply(Math,persArr.map(function(persArr){return persArr.age}));
+        },
+        averageAge: function (){
+            return Math.round(persArr.map(function(persArr){return persArr.age}).reduce(function(a, b){return a+b})/persArr.length);
+        },
+        names: function(){
+            return persArr.map(function(persArr){return persArr.name}).sort(function(a,b){return a.localeCompare(b)}).join(", ").toString();
+        }
+    };
+    
+    console.log(persons.names());
+    return {minAge: persons.minAge(),maxAge: persons.maxAge(),averageAge: persons.averageAge(), names: persons.names()};
+};
 
-	// Din kod här...
 
-}
-
+// var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+// var result = makePerson(data);
+// console.log(result);
